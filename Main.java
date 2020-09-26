367ad8ec007f90e19e273f324df9118308381f6e
Jareen Mahmood <jtm150330@utdallas.edu>
2020-09-26 17:51:56 -0500

    I edited the code inside Main.java according to the instructions on hw 1 for CE3354.006
    I changed the number of arguments to add inside the code
    
    *Changes were made to the java code
    
    Closes-Bug: #335401
    -wq

---
+++ Main.java
@@ -0,0 +1,17 @@
+package adder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            int result = addArguments(args);
+            System.out.println(result);
+        } catch (Exception e) {
+            System.err.println("Please provide four integers to add");
+        }
+    }
+
+    private static int addArguments(String[] args) {
+        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]) +  Integer.valueOf(args[2]) +  	Integer.valueOf(args[3]);
+    }
+}
