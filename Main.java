0332099a48d071050aa6c8e7e665b2237a2f11d3
Jareen Mahmood <jtm150330@utdallas.edu>
2020-09-26 18:14:23 -0500

    I changed the adder-Arguments method in Main.java file
    I made changed according to the HW1 instructions in CE 3354.006
    course.
    
    * This changes are made according to number 10 on the hw1 instructions
    
    Closes-Bug: #006092620
    
    -wq

--- Main.java
+++ Main.java
@@ -39,6 +39,12 @@ public class Main {
        }
 
     private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]) +  Integer.valueOf(args[2]) +  	Integer.valueOf(args[3]);
+       
+	if (args[0] == "-");
+	{
+		Integer.valueOf(args[0]) = -1;
+		return (Integer.valueOf(args[0]) * (Integer.valueOf(args[1]) +  Integer.valueOf(args[2]) + Integer.valueOf(args[3])));
+	} 
+	return Integer.valueOf(args[0]) + Integer.valueOf(args[1]) +  Integer.valueOf(args[2]) +  	Integer.valueOf(args[3]);
     }
 }
