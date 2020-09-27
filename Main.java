b6e845efefb9c121afe71a1cb3ac446f5ef3c847
Jareen Mahmood <jtm150330@utdallas.edu>
2020-09-26 20:14:07 -0500

    I had to change the main method on the Main.java file this time according to the assignment
    I had to switch back to master branch to make changes
    
    * I did these as hw 1 assignment of ce3354.006 requires to.
    
    Closes-Bug: #33540062020
    
    -wq

--- Main.java
+++ Main.java
@@ -1,15 +1,42 @@
 package adder;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+    	
+    	Scanner  input = new Scanner(System.in);
+    	String sn = "\0";
+    	int i = 0;
+    	
+    	while (i < 4)
+    	{
+    		sn = input.next();
+    		args[i] = sn;
+    		
+    		i++;
+    	}
+    	
         try {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
             System.err.println("Please provide four integers to add");
         }
-    }
+        
+         if (i >= 4)
+         {
+        	 System.err.println("Please limit your inputs to four integer");
+         }
+         
+         if ((sn == null) || (sn != "-"))
+         {
+        	 System.err.println ("Please enter a valid intger.");
+         }
+        		 
+        input.close();
+       }
 
     private static int addArguments(String[] args) {
         return Integer.valueOf(args[0]) + Integer.valueOf(args[1]) +  Integer.valueOf(args[2]) +  	Integer.valueOf(args[3]);
